d0fb49ed8c093f7b166e8f7710933dd48c51bf96
林士立 <t301000@gmail.com>
2018-07-04 21:22:56 +0800

    註解 mysql port 設定部份

--- docker/setup-env.sh
+++ docker/setup-env.sh
@@ -39,16 +39,16 @@ if [[ "$value" != "" ]]; then
     MYSQL_ROOT_PASSWORD=$value
 fi
 
-value=""
-read -p "資料庫 port：(預設： 3306)  " value
-if [[ "$value" != "" ]]; then
-    MYSQL_PORT=$value
-fi
+#value=""
+#read -p "資料庫 port：(預設： 3306)  " value
+#if [[ "$value" != "" ]]; then
+#    MYSQL_PORT=$value
+#fi
 
 echo "MYSQL_DATABASE=${MYSQL_DATABASE}" >> .env
 echo "MYSQL_USER=${MYSQL_USER}" >> .env
 echo "MYSQL_PASSWORD=${MYSQL_PASSWORD}" >> .env
 echo "MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}" >> .env
-echo "MYSQL_PORT=${MYSQL_PORT}" >> .env
+#echo "MYSQL_PORT=${MYSQL_PORT}" >> .env
 
 printf "\n>>> .env 設定完成！！\n\n"
