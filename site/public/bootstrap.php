aee96d2dca4fb3d1e08a969500bcd85ede1d7d14
林士立 <t301000@gmail.com>
2018-07-14 21:44:25 +0800

    預設 PDO:: ATTR_EMULATE_PREPARES 為 true

--- site/public/bootstrap.php
+++ site/public/bootstrap.php
@@ -22,7 +22,8 @@
     try {
         $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE . ";charset=" . DB_CHARSET . ";port=" . DB_PORT;
         $pdo = new PDO($dsn, DB_USER, DB_PASSWORD);
-        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        // 關閉模擬 prepare，確定資料庫有支援 prepare 才啟用
+        // $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
