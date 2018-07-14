a19f00f511856acbe8c533d20046237b019313cb
林士立 <t301000@gmail.com>
2018-07-14 21:54:48 +0800

    pdo emulate prepares 設定移到 config/system.php

--- site/public/config/system.php
+++ site/public/config/system.php
@@ -19,19 +19,5 @@
     // 字元編碼
     define('DB_CHARSET', 'utf8');
 
-    // // 用於 OpenID
-    // define('DOMAIN_NAME', 'localhost');
-    // // OpenID 取得欄位
-    // define('OPENID_REQUIRED', [
-    //     // 'namePerson/friendly', // 暱稱
-    //     'contact/email', // email
-    //     'namePerson', // 姓名
-    //     // 'birthDate', // 生日，1985-06-12
-    //     'person/gender', // 性別，M 男
-    //     // 'contact/postalCode/home', // 識別碼
-    //     // 'contact/country/home', // 單位簡稱，xx國中
-    //     'pref/language', // 年級班級座號，6 碼
-    //     'pref/timezone' // 授權資訊，含單位代碼、單位全銜、職務別、職稱別、身份別等資料，陣列
-    // ]);
-    // // 是否使用假資料，須同時開啟除錯才有效
-    // define('FAKE_OPENID_USER', true);
+    // 是否關閉模擬 prepare，確定資料庫有支援 prepare 才設為 true
+    define('PDO_EMULATE_PREPARES', false);
