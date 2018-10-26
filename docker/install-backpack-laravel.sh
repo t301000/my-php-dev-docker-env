e589dcbcab1c81f6d1b25e9c5a9ead782624949b
林士立 <t301000@gmail.com>
2018-10-26 22:48:31 +0800

    User Model 加入 trait，修改 config/auth.php users provider 之 model

--- docker/install-backpack-laravel.sh
+++ docker/install-backpack-laravel.sh
@@ -5,6 +5,38 @@
 ###################################
 #!/bin/bash
 
+# add Trait to User Model
+function addTraitToUserModel () {
+    read -p "User Model 相對於 app 之路徑含檔名？ (預設： User.php) " path
+    if [[ $path == "" ]]; then
+        path='User.php'
+    fi
+
+    echo ""
+    echo ">>>> 在 app/$path 中加入 Traits： CrudTrait、HasRoles"
+
+    trait1='use Backpack\\CRUD\\CrudTrait;'
+    trait2='use Spatie\\Permission\\Traits\\HasRoles;'
+    sed -i "/namespace App*/a \\\n${trait1}\n${trait2}" ../site/app/$path
+    trait1='    use CrudTrait;'
+    trait2='    use HasRoles;'
+    sed -i "/use Notifiable;/a \\\n${trait1}\n${trait2}" ../site/app/$path
+}
+
+# 修改 config/auth.php 中 users provider 之 model
+function modifyAuthUserModel () {
+    echo ""
+    echo ">>>> 修改 config/auth.php 中 users provider 之 model"
+    echo ">>>>   App\User::class 改為 Backpack\Base\app\Models\BackpackUser::class"
+
+    src="'model' => App\\\User"
+    to="'model' => Backpack\\\Base\\\app\\\Models\\\BackpackUser"
+    sed -i "s/${src}/${to}/g" ../site/config/auth.php
+}
+
+
+###############################
+
 ./composer-php-fpm.sh require backpack/crud
 sleep 5
 
@@ -41,6 +73,12 @@ if [[ $ans == "" ]] || [[ $ans == "Y" ]] || [[ $ans == "y" ]]; then
     wget https://campus-xoops.tn.edu.tw/uploads/tad_book3/file/34/permissionmanager-zh-TW.zip
     unzip permissionmanager-zh-TW.zip
     mv zh-TW/permissionmanager.php ../../site/resources/lang/vendor/backpack/zh-TW/ && cd .. && rm -rf tmp
+
+    read -p "是否在 User Model 中加入 Trait 並修改 config/auth.php？ [Y/n] " ans
+    if [[ $ans == "" ]] || [[ $ans == "Y" ]] || [[ $ans == "y" ]]; then
+        addTraitToUserModel
+        modifyAuthUserModel
+    fi
 
     echo "其餘調整請參考："
     echo "https://campus-xoops.tn.edu.tw/modules/tad_book3/page.php?tbdsn=1155"
