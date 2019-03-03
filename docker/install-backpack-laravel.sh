00e0397470072c3eaba7c8f902dc75df1431caa5
林士立 <t301000@gmail.com>
2019-03-03 21:05:32 +0800

    調整變數

--- docker/install-backpack-laravel.sh
+++ docker/install-backpack-laravel.sh
@@ -5,13 +5,16 @@
 ###################################
 #!/bin/bash
 ver="3.6"
-read -p "backpack/crud 版本？  (預設： 3.6)  [3.4 / 3.5 / 3.6] " ver
-if [[ $ver == "" ]] || [[ $ver == "3.5" ]] || [[ $ver == "3.6" ]]; then
-    echo ">>>> 正在安裝 backpack/crud:$ver.*"
+read -p "backpack/crud 版本？  (預設： 3.6)  [3.4 / 3.5 / 3.6] " my_ver
+if [[ $my_ver == "" ]] || [[ $my_ver == "3.5" ]] || [[ $my_ver == "3.6" ]]; then
+    if [[ $my_ver != "" ]]; then
+        ver=$my_ver
+    fi
+    echo ">>>> 正在安裝 backpack/crud:${ver}.*"
     ./install-backpack-crud-3.5.sh
 fi
 
-if [[ $ver == "3.4" ]]; then
+if [[ $my_ver == "3.4" ]]; then
     echo ">>>> 正在安裝 backpack/crud:3.4.*"
     ./install-backpack-crud-3.4.sh
 fi
